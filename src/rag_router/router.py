29fad9b8f5fa6d2b30f664a781d680398e4fe329
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-06-18 09:41:40 +0100

    Update generative_search endpoint

--- src/rag_router/router.py
+++ src/rag_router/router.py
@@ -7,10 +7,8 @@ router = APIRouter()
 
 
 # We assume that the Weaviate collection is configured with an LLM.
-WEAVIATE_HOST = (
-    "localhost" if os.getenv("WEAVIATE_HOST") is None else os.getenv("WEAVIATE_HOST")
-)
-WEAVIATE_COLLECTION_NAME = "WeaviateTextChunks"
+WEAVIATE_HOST = os.getenv("WEAVIATE_HOST") or "localhost"
+WEAVIATE_COLLECTION_NAME = os.getenv("WEAVIATE_COLLECTION_NAME") or "WeaviateTextChunks"
 
 
 client = weaviate.connect_to_local(host=WEAVIATE_HOST)
@@ -19,4 +17,5 @@ collection = client.collections.get(WEAVIATE_COLLECTION_NAME)
 
 @router.get("/generative_search/{query}")
 def generative_search(query: str):
-    collection.generate.near_text(query=query, limit=5, grouped_task=query)
+    response = collection.generate.near_text(query=query, limit=5, grouped_task=query)
+    return response.generated
