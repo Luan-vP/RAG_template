c03b90c2d96c815cc2409d042c17572f820c0adb
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-06-20 20:04:18 +0100

    Add collection clearing script

---
+++ scripts/clear_weaviate_collection.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+import weaviate
+
+WEAVIATE_COLLECTION_NAME = os.getenv("WEAVIATE_COLLECTION_NAME") or "WeaviateTextChunks"
+
+client = weaviate.connect_to_local(host="localhost")
+
+client.collections.delete(WEAVIATE_COLLECTION_NAME)
+
+print(f"{WEAVIATE_COLLECTION_NAME} collection deleted.")
