fc5a73c67cc721bd7b7fad91a24e741777cd4ba3
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-06-21 15:56:24 +0100

    Prevent db connection on module import

--- src/rag_router/router.py
+++ src/rag_router/router.py
@@ -7,15 +7,19 @@ from pydantic import BaseModel
 router = APIRouter()
 
 
-# We assume that the Weaviate collection is configured with an LLM.
-WEAVIATE_HOST = os.getenv("WEAVIATE_HOST") or "localhost"
-WEAVIATE_COLLECTION_NAME = (
-    os.getenv("WEAVIATE_COLLECTION_NAME") or "WeaviateTextChunks"
-)
+def get_collection():
 
+    WEAVIATE_HOST = os.getenv("WEAVIATE_HOST") or "localhost"
+    WEAVIATE_COLLECTION_NAME = (
+        os.getenv("WEAVIATE_COLLECTION_NAME") or "WeaviateTextChunks"
+    )
+    client = weaviate.connect_to_local(host=WEAVIATE_HOST)
+    collection = client.collections.get(WEAVIATE_COLLECTION_NAME)
 
-client = weaviate.connect_to_local(host=WEAVIATE_HOST)
-collection = client.collections.get(WEAVIATE_COLLECTION_NAME)
+    try:
+        yield collection
+    finally:
+        client.close()
 
 
 class Query(BaseModel):
@@ -24,7 +28,8 @@ class Query(BaseModel):
 
 @router.post("/generative_search")
 def generative_search(input: Query):
-    response = collection.generate.near_text(
-        query=input.query, limit=5, grouped_task=input.query
-    )
-    return response.generated
+    with get_collection() as collection:
+        response = collection.generate.near_text(
+            query=input.query, limit=5, grouped_task=input.query
+        )
+        return response.generated
